58bd40ada8b3ae5c7a8706f57e053e7356351fc9
Kitsune-fx <kant.cmd@gmail.com>
2021-10-31 10:39:10 +0700

    [🧨 Third commit]

--- backend/routes/department.js
+++ backend/routes/department.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Router = express.Router();
 const dbCon = require('../connection.js')
 
-Router.get('/api' , (req,res)=>{
+Router.get('/' , (req,res)=>{
     console.log('get dep');
 
     dbCon.query('SELECT * FROM department', (error, results, field)=>{
@@ -20,6 +20,80 @@ Router.get('/api' , (req,res)=>{
         }
 
     })
+})
+
+Router.post('/' ,(req, res)=>{
+    console.log('post dep')
+    const name = req.body.name;
+    if(!name){
+        return res.status(400).send({error: true , message: 'Please enter data'});
+    }
+    
+    dbCon.query('INSERT INTO department( department_name ) VALUES(?)', name, (error, results, field)=>{
+        if(!error){
+            return res.status(200).send({error: false, data: results, message: 'Data added'})
+        }
+
+    })
+
+})
+
+Router.get('/:id', (req,res)=>{
+    console.log('search dep');
+    const id = req.params.id;
+   
+    if(!id){
+        return res.status(400).send({error: true, message: "Please enter id"})
+    } 
+
+    dbCon.query("SELECT * FROM department WHERE department_id = ?", id, (error, results, field)=>{
+        if(!error){
+            let message = ""
+            if(results === undefined || results.length == 0){
+                message = "No data"
+            }
+            else{
+                message = "Success got data"
+            }
+            return res.status(200).send({error: false, data: results, message: message})
+
+        }else throw error;
+        
+    })
+})
+
+Router.put('/', (req, res)=>{
+    console.log('put dep')
+    const name = req.body.name
+    const id = req.body.id;
+
+    if(!name || !id){
+        return res.status(400).send({error: false, message: "Please enter data"});
+    }
+
+    dbCon.query("UPDATE department SET department_name = ? WHERE department_id = ?",[name, id], (error, results, field)=>{
+        if(!error){
+            return res.status(200).send({error: false, data: results, message:"Update success"})
+        }
+        else throw error;
+
+    })
+})
+
+Router.delete('/:id', (req,res)=>{
+    console.log('delete dep')
+    const id = req.params.id;
+    if(!id){
+        return res.status(400).send({error: true, message: "Please enter id"});
+    }
+
+    dbCon.query('DELETE FROM department WHERE department_id = ?', id, (error, results, field)=>{
+        if(!error){
+            return res.status(200).send({error: false, data: results, message: "Data deleted"})
+        }
+        else throw error
+    })
+
 })
 
 module.exports = Router ;
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,15 @@ const departmentRoute = require('./routes/department')
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+//middleware
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+})
+
 app.use('/employee/api', employeeRoute);
 app.use('/department/api', departmentRoute)
 
